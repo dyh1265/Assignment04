1233fa8cfb09afe253a623b95ea61d1591bf3994
dyh1265 <dyh@list.ru>
2017-09-19 21:33:58 +0200

    Finished project setting

---
+++ cgdoc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cgdoc
+//
+//  Created by Андрей Сиразитдинов on 19/09/2017.
+//  Copyright © 2017 Андрей Сиразитдинов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
